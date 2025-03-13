514c139c3f7f4219556f4fcff30d4d8561587cac
rizkiariffudin <rizki.ariff3@gmail.com>
2025-03-13 12:51:25 +0700

    Fix Response DTO

--- resource/src/main/java/gpl/karina/resource/restdto/response/ResourceResponseDTO.java
+++ resource/src/main/java/gpl/karina/resource/restdto/response/ResourceResponseDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class ResourceResponseDTO {
+    private Long id;
     private String resourceName;
     private String resourceDescription;
     private String resourceSupplier;
