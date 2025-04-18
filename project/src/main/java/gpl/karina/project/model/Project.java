c4e3b41eda53c199affd31a469c270e7c8c33cb4
wirr <prawirarzkn94@gmail.com>
2025-04-18 19:36:07 +0700

    pbi-34: Log for create and update status done, Waiting for update project detail to be implemented

--- project/src/main/java/gpl/karina/project/model/Project.java
+++ project/src/main/java/gpl/karina/project/model/Project.java
@@ -2,7 +2,7 @@ package gpl.karina.project.model;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.DiscriminatorType;
@@ -10,7 +10,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
-
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -18,6 +19,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import java.util.Date;
+import java.util.List;
 
 
 
@@ -64,5 +66,7 @@ public class Project {
     @Column(name = "total_pemasukkan")
     private Long projectTotalPemasukkan;
 
-    
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "project_id")
+    List<LogProject> projectLogs;
 }
