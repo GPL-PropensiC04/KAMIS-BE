fcf85c27749d5b972779b6f9c62d183cb19efa7f
Muhammad Pendar Bintang Kasdiono <muhammad.pendar@ui.ac.id>
2025-04-18 13:29:55 +0700

    Add: List All Projects with Filtering for projects Id, Status, type, nama, clientId, start and endDate. Also update Add Project to store Total Pemasukkan for Distribution.  and Rename DTO for returning list of All Projects

--- project/src/main/java/gpl/karina/project/restservice/ProjectService.java
+++ project/src/main/java/gpl/karina/project/restservice/ProjectService.java
@@ -2,9 +2,15 @@ package gpl.karina.project.restservice;
 
 import gpl.karina.project.restdto.request.ProjectRequestDTO;
 import gpl.karina.project.restdto.response.ProjectResponseWrapperDTO;
-
+import gpl.karina.project.restdto.response.listProjectResponseDTO;
+import java.util.List;
+import java.util.Date;
 public interface ProjectService {
     public ProjectResponseWrapperDTO addProject(ProjectRequestDTO projectRequestDTO) throws Exception;
-    // public List<ProjectResponseDTO> getAllProject() throws Exception;
+    public List<listProjectResponseDTO> getAllProject(
+        String idSearch, String projectStatus, String projectType,
+        String projectName, String projectClientId, Date projectStartDate,
+        Date projectEndDate
+    ) throws Exception;
     // public ProjectResponseDTO getProjectById(String id) throws Exception;
 }
