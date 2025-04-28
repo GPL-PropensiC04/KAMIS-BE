2b736a629f65ee392017f359f433ba0f8cfe9516
Rakha Fadil <117321552+Fadeeaal@users.noreply.github.com>
2025-04-28 08:51:27 +0700

    [UPDATE] Connect w/ last maintenance (#44)

--- asset/src/main/java/gpl/karina/asset/service/AssetService.java
+++ asset/src/main/java/gpl/karina/asset/service/AssetService.java
@@ -2,13 +2,15 @@ package gpl.karina.asset.service;
 
 import java.util.List;
 import java.util.UUID;
+
+import gpl.karina.asset.dto.response.AssetListResponseDTO;
 import gpl.karina.asset.dto.response.AssetResponseDTO;
 import gpl.karina.asset.model.Asset;
 import gpl.karina.asset.dto.request.AssetAddDTO;
 import gpl.karina.asset.dto.request.AssetUpdateRequestDTO;
 
 public interface AssetService {
-    List<AssetResponseDTO> getAllAsset();
+    List<AssetListResponseDTO> getAllAsset();
     AssetResponseDTO getAssetById(String id) throws Exception;
     AssetResponseDTO updateAssetImage(String id, byte[] imageData) throws Exception;
     void deleteAsset(String id) throws Exception;
@@ -16,4 +18,5 @@ public interface AssetService {
     AssetResponseDTO addAsset(AssetAddDTO assetAddDTO);
     Asset getAssetFoto(String id);
     List<AssetResponseDTO> getAssetsBySupplier(UUID supplierId);
+
 }
